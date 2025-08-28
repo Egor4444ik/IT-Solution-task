d381ca1c40c93aba3085d4cbf0148ffb8b8b6149
Егор Смирнов <egor4e4ik@gmail.com>
2025-08-29 01:11:36 +0300

    urls fixes

--- solution_site/solution_site/urls.py
+++ solution_site/solution_site/urls.py
@@ -4,6 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('random_quotes.urls')),
+    path('random_quotes/', include('random_quotes.urls')),
     path('admin/', admin.site.urls),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
