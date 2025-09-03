a62ce0f1906a311f9179fcdc8e53e361a732637e
Егор Смирнов <egor4e4ik@gmail.com>
2025-09-03 16:37:51 +0300

    remove 1 peacee of path in settings

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 cd /app/solution_site/
 
 echo "Applying database migrations..."
--- solution_site/solution_site/wsgi.py
+++ solution_site/solution_site/wsgi.py
@@ -3,7 +3,7 @@ WSGI config for solution_site project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For more information o n this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'solution_site.solution_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'solution_site.settings')
 
 application = get_wsgi_application()
